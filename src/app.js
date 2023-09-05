0c5ad61527848137449aea7f1d4ed4dfed498db5
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-05 11:00:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,15 +8,44 @@ const productNames = JSON.parse(fs.readFileSync(`${__dirname}/data/names.json`))
 //Middleware
 app.use(express.json());
 
-// GET endpoint for sending the product to client by id
+// GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
 app.get("/api/v1/names/:id", (req, res) => {
 	try {
+		// const productNames = JSON.parse(fs.readFileSync(`${__dirname}/data/names.json`));
 		// console.log(productNames[2]);
+
 		// productNames.map((item) => {
 		// 	console.log(item);
 		// });
-		const productId = parseInt(req.params.id);
+
+		const paramId = Number(req.params.id);
+		const product = productNames.find((p) => p.id === paramId);
+		// console.log(paramId);
+
+		if (typeof paramId !== "number") {
+			return res.status(400).json({ error: "Invalid data types" });
+		}
+
+		if (!product) {
+			return res.status(404).json({ status: "failed", message: "Not found!" });
+		} else if (product) {
+			res.status(200).json({
+				status: "success",
+				message: "Product Name fetched successfully",
+				data: { product },
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+});
+
+module.exports = app;
+
+/**
+ * const productId = parseInt(req.params.id);
 
 		// Find the product by ID
 		const product = productNames.find((p) => p.id === productId);
@@ -38,10 +67,4 @@ app.get("/api/v1/names/:id", (req, res) => {
 				message: "Not found!",
 			});
 		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: "Internal server error" });
-	}
-});
-
-module.exports = app;
+ */
