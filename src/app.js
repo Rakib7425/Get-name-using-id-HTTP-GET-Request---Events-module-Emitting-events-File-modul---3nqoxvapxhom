c9984e941f9dc1370bb25ef2e14ca05232cbf64c
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-06 22:21:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,13 +12,6 @@ app.use(express.json());
 //Endpoint - /api/v1/names/:id
 app.get("/api/v1/names/:id", (req, res) => {
 	try {
-		// const productNames = JSON.parse(fs.readFileSync(`${__dirname}/data/names.json`));
-		// console.log(productNames[2]);
-
-		// productNames.map((item) => {
-		// 	console.log(item);
-		// });
-
 		const paramId = Number(req.params.id);
 		const product = productNames.find((p) => p.id === paramId);
 		// console.log(paramId);
@@ -49,28 +42,3 @@ app.get("/api/v1/names/:id", (req, res) => {
 });
 
 module.exports = app;
-
-/**
- * const productId = parseInt(req.params.id);
-
-		// Find the product by ID
-		const product = productNames.find((p) => p.id === productId);
-
-		if (product) {
-			// Object found, return with a status code of 200
-			res.status(200).json({
-				status: "success",
-				message: "Product Name fetched successfully",
-				data: {
-					id: product.id,
-					name: product.name,
-				},
-			});
-		} else {
-			// Object not found, return with a status code of 404
-			res.status(404).json({
-				status: "failure",
-				message: "Not found!",
-			});
-		}
- */
